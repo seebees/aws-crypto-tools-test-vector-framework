f831718d8419e92c86660e58ea4288070471b065
Robin Salkeld <salkeldr@amazon.com>
2021-05-22 08:06:22 -0700

    Typo: “error_message” -> “error-message”

--- features/0006-awses-message-decryption-generation-generate.py
+++ features/0006-awses-message-decryption-generation-generate.py
@@ -209,7 +209,7 @@ def _build_mrk_tests(keys):
                 "encryption-scenario": mrk_encryption_scenario,
                 "decryption-master-keys": [bad_master_key],
                 "result": {
-                    "error_message": "Mismatched master key: " + str(bad_master_key)
+                    "error-message": "Mismatched master key: " + str(bad_master_key)
                 }
             },
         )
